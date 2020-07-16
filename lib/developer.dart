a9e7731baf1ce4c84d5eec7b5c58f9a87b95f1fa
Im-awesome-Aadi <43954262+Im-awesome-Aadi@users.noreply.github.com>
2020-07-16 14:10:40 +0530

    Update developer.dart

--- lib/developer.dart
+++ lib/developer.dart
@@ -9,21 +9,24 @@ class dev extends StatelessWidget {
       ),
       body: Center(
         child: Container(
+          padding: EdgeInsets.all(10),
           height: 450,
           width: 350,
           child: Center(
-            child: Card(
-              margin: EdgeInsets.all(10),
-              color: Colors.grey[900],
-              child: Container(
-                padding: EdgeInsets.all(10),
-                child: Column(
-                  children: <Widget>[
-                    Center(child: Text("Hi, I'm Aditya Maheshwari prefinal year student of computer science engineering and sole Developer of this App. I had developed this app just to brush up My App development Skills. ",style: TextStyle(fontFamily: "Girassol",fontWeight: FontWeight.bold,fontSize: 20, letterSpacing: 1.2),)),
-                    SizedBox(height: 25,),
-                    Center(child: Text("Hope u Guys will like this App. Any Suggestion is highly Welcomed. Enjoy the App",style: TextStyle(fontFamily: "Girassol",fontWeight: FontWeight.bold,fontSize: 20, letterSpacing: 1.5),))
-                  ],
-                ),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.grey[900],
+
+              ),
+              padding: EdgeInsets.all(10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Center(child: Text("Hi, I'm Aditya Maheshwari prefinal year student of computer science engineering and sole Developer of this App. I had developed this app just to brush up My App development Skills. ",style: TextStyle(fontFamily: "Girassol",fontWeight: FontWeight.bold,fontSize: 20, letterSpacing: 1.5),)),
+                  SizedBox(height: 25,),
+                  Center(child: Text("Hope u Guys will like this App. Any Suggestion is highly Welcomed. Enjoy the App",style: TextStyle(fontFamily: "Girassol",fontWeight: FontWeight.bold,fontSize: 20, letterSpacing: 1.5),))
+                ],
               ),
             ),
           ),
